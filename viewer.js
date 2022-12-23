f0fbaa0d0827d30bb69855a3dc3360d4dc1cbbd4
LaserButik <121310325+LaserButik@users.noreply.github.com>
2022-12-23 23:55:39 +0100

    Delete viewer.js

--- viewer.js
+++
@@ -1,4 +0,0 @@
-chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-  // Display the extracted contact information in the viewer window
-  document.getElementById("contact").innerText = message.contact;
-});
