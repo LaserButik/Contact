8df3f650899dedd0ff2fcd5754c6b95f38e7166d
LaserButik <121310325+LaserButik@users.noreply.github.com>
2022-12-23 22:06:02 +0100

    Update background.js

--- background.js
+++ background.js
@@ -14,3 +14,27 @@ function extractContact(text) {
   // Replace this with your own code to extract the contact information from the webpage
   return "Contact information not found";
 }
+chrome.browserAction.onClicked.addListener(function(tab) {
+  chrome.tabs.executeScript({
+    code: 'document.body.innerText'
+  }, function(results) {
+    var text = results[0];
+    // Extract the customer contact information from the webpage using regular expressions or other methods
+    var contact = extractContact(text);
+    // Open a viewer window to display the extracted contact information
+    chrome.windows.create({
+      url: "viewer.html",
+      type: "popup",
+      width: 400,
+      height: 300
+    }, function(window) {
+      // Pass the extracted contact information to the viewer window
+      chrome.tabs.sendMessage(window.tabs[0].id, {contact: contact});
+    });
+  });
+});
+
+function extractContact(text) {
+  // Replace this with your own code to extract the contact information from the webpage
+  return "Contact information not found";
+}
